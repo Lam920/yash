9eed5ac5bcdf8f91669e6da91999557a8a2632c1
Lam920 <godhandxvw@gmail.com>
2024-09-23 23:11:29 +0700

    [YASH-2] Init cmd before exec

--- parsecmd.c
+++ parsecmd.c
@@ -1,8 +1,110 @@
 #include "parsecmd.h"
-int paresecmd(char cmd[])
+
+char delimiter[DELIMITER_NUM][DELIMITER_MAXLENGTH] = {"|", "2>", "<", ">", "&"};
+
+static int getcmd_type(char delimiter[DELIMITER_NUM][DELIMITER_MAXLENGTH], char *input, int delimeter_num)
 {
+    int i = 0, type_idx, type_cmd;
+    for (i = 0; i < delimeter_num; i++)
+    {
+        if (strstr(input, delimiter[i]))
+        {
+            type_idx = i;
+            printf("Get delimeter: %s\n", delimiter[i]);
+            break;
+        }
+    }
+
+    switch (type_idx)
+    {
+    case 0:
+        type_cmd = PIPE;
+        break;
+    case 1:
+    case 2:
+    case 3:
+        type_cmd = FILE_REDIRECTION;
+        break;
+    case 4:
+        type_cmd = BACKGROUND;
+        break;
+    default:
+        type_cmd = EXEC;
+        break;
+    }
+
+    return type_cmd;
 }
 
-void *init_cmd(int cmd_type)
+
+int parsercmd(char *input_cmd)
 {
+    int type_cmd = getcmd_type(delimiter, input_cmd, DELIMITER_NUM);
+    struct cmd *cmd = init_cmd(type_cmd);
+    struct exec_cmd *ecmd;
+    struct file_cmd *fcmd;
+    struct pipe_cmd *pcmp;
+
+    switch (type_cmd)
+    {
+    case EXEC:
+        ecmd = (struct exec_cmd*)cmd;
+        do_execcmd(ecmd);
+        break;
+    default:
+        break;
+    }
+}
+
+void do_execcmd(struct exec_cmd *ecmd){
+    char binexec[MAXLENGTH_CMD] = {0};
+
+}
+
+struct cmd* init_execcmd()
+{
+    struct exec_cmd *exec_cmd = (struct exec_cmd *)malloc(sizeof(struct exec_cmd));
+    
+    exec_cmd->type = EXEC;
+    return (struct cmd*)exec_cmd;
+}
+
+struct cmd* init_filecmd()
+{
+    struct file_cmd *file_cmd = (struct file_cmd *)malloc(sizeof(struct file_cmd));
+    memset(file_cmd, 0, sizeof(struct file_cmd));
+    file_cmd->type = FILE_REDIRECTION;
+    return (struct cmd*)file_cmd;
+}
+
+struct cmd* init_pipecmd()
+{
+    struct pipe_cmd *pipe_cmd = (struct file_cmd *)malloc(sizeof(struct pipe_cmd));
+    memset(pipe_cmd, 0, sizeof(struct pipe_cmd));
+    pipe_cmd->type = FILE_REDIRECTION;
+    return (struct cmd*)pipe_cmd;
+}
+
+struct cmd* init_cmd(int type){
+    struct cmd *run_cmd = NULL;
+    switch (type)
+    {
+    case EXEC:
+        printf("Init exec cmd\n");
+        run_cmd = init_execcmd();
+        break;
+    case FILE_REDIRECTION:
+        printf("Init file redirection cmd\n");
+        run_cmd = init_filecmd();
+        break;
+    case PIPE:
+        printf("Init pipe cmd\n");
+        run_cmd = init_pipecmd();
+        break;
+    default:
+        printf("Init nothing cmd\n");
+        break;
+    }
+
+    return run_cmd;
 }
--- parsecmd.h
+++ parsecmd.h
@@ -1,28 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
 #define EXEC 1
 #define FILE_REDIRECTION 2
-#define PIPING 3
+#define PIPE 3
 #define BACKGROUND 4
+#define DELIMITER_NUM 5
+#define DELIMITER_MAXLENGTH 3
 #define MAXLENGTH_CMD 2000
+#define MAX_ARGC 10
 
 #define NUM_OF_PIPE 1
 
-int paresecmd(char cmd[]);
+int parsercmd(char *input_cmd);
 
-int init_cmd(int type_cmd);
+struct cmd* init_cmd(int type_cmd);
+
+struct cmd {
+    int type;
+};
+
+typedef struct cmd cmd;
 
 struct exec_cmd
 {
     int type;
     char cmd_exec[MAXLENGTH_CMD];
-    char **cmd_argv;
+    char *argv[MAX_ARGC];
+    char *eargv[MAX_ARGC];
     int argc;
 };
 
 struct file_cmd
 {
     int type;
-    char file_name[16];
     char cmd_exec[MAXLENGTH_CMD];
+    char file_name[16];
     int fd; // stdin, stdout, stderr
 };
 
@@ -32,6 +47,6 @@ struct pipe_cmd
     char cmd[NUM_OF_PIPE + 1][MAXLENGTH_CMD / 2];
 };
 
-typedef struct file_cmd file_cmd;
-typedef struct pipe_cmd pipe_cmd;
-typedef struct exec_cmd exec_cmd;
+typedef struct file_cmd file_cmd_t;
+typedef struct pipe_cmd pipe_cmd_t;
+typedef struct exec_cmd exec_cmd_t;
